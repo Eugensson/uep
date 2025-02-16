d9836fc6ec11e45821dd5ce5fcd158cf82e6ecff
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-02-16 15:47:17 +0200

    Created asynchronous allback func in auth.ts

--- auth.ts
+++ auth.ts
@@ -2,7 +2,11 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import authConfig from "@/auth.config";
+
 import { prisma } from "@/prisma/prisma";
+
+import { getUserById } from "@/data/user";
+import { getAccountByUserId } from "@/data/account";
 
 export const {
   auth,
@@ -13,4 +17,43 @@ export const {
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider !== "credentials") return true;
+
+      const existingUser = await getUserById(user.id ?? "");
+
+      if (!existingUser) return false;
+
+      if (!existingUser?.emailVerified) return false;
+
+      return true;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.isOauth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
+      token.image = existingUser.image;
+
+      return token;
+    },
+    async session({ session, token }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.sub,
+          isOauth: token.isOauth,
+        },
+      };
+    },
+  },
 });
